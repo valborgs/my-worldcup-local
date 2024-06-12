5ec8a431d4825fe63d6fc4732df115c75e2fc58c
valborgs <voll1212@naver.com>
2024-06-13 02:51:29 +0900

    [Feat] 로딩 이미지 추가

--- lib/screens/result_worldcup_screen.dart
+++ lib/screens/result_worldcup_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:my_worldcup_local/screens/play_worldcup_screen.dart';
 
 import '../api/imgbb_upload.dart';
@@ -31,6 +32,14 @@ class _ResultWorldCupScreen extends State<ResultWorldCupScreen> {
     _confettiController.dispose();
   }
 
+  // 로딩 이미지
+  final spinkit = const SpinKitPulsingGrid(
+    color: Colors.deepPurple,
+    size: 100.0,
+  );
+
+  var isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     _confettiController = ConfettiController(duration: const Duration(seconds: 5));
@@ -42,132 +51,142 @@ class _ResultWorldCupScreen extends State<ResultWorldCupScreen> {
         title: Text("${widget.worldCupModel.title} 우승자"),
       ),
       // 화면
-      body: Container(
-        height: double.maxFinite,
-        color: Colors.grey.withOpacity(0.1),
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: const Row(
-                children: [
-                  Text(
-                    "축하합니다!",
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                  Icon(Icons.auto_awesome, color: Colors.yellow,)
-                ],
-              ),
-            ),
-            const Padding(padding: EdgeInsets.only(top: 20)),
-            SizedBox(
-              width: 150,
-              height: 150,
-              child: CircleAvatar(
-                backgroundImage: Image.file(File(widget.winnerModel.imagePath)).image,
-              ) ,
-            ),
-            Text(
-              widget.winnerModel.imageInfo,
-              style: const TextStyle(
-                fontSize: 18,
-              ),
-            ),
-            const Padding(padding: EdgeInsets.only(top: 30)),
-            // 버튼 묶음
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(
+            height: double.maxFinite,
+            color: Colors.grey.withOpacity(0.1),
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            alignment: Alignment.center,
+            child: Column(
               children: [
-                // 다시하기 버튼
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => PlayWorldCupScreen(widget.worldCupModel, widget.round),
-                      ),
-                    );
-                  },
-                  style: const ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.red),
-                      shape: WidgetStatePropertyAll(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(5.0))
-                          )
-                      )
-                  ),
+                Container(
+                  alignment: Alignment.center,
                   child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(
-                        Icons.replay,
-                        color: Colors.white,
-                      ),
-                      Padding(padding: EdgeInsets.only(right: 10)),
                       Text(
-                        '다시 하기',
+                        "축하합니다!",
                         style: TextStyle(
-                          color: Colors.white,
+                          fontSize: 22,
                         ),
-                      )
+                      ),
+                      Icon(Icons.auto_awesome, color: Colors.yellow,)
                     ],
                   ),
                 ),
-                const Padding(padding: EdgeInsets.only(top: 10)),
-                // 공유 버튼
-                IconButton(
-                    onPressed: () => shareGameWithKakao(),
-                    icon: Image.network(
-                      'https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png',
-                      width: 40,
-                    )
+                const Padding(padding: EdgeInsets.only(top: 20)),
+                SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: CircleAvatar(
+                    backgroundImage: Image.file(File(widget.winnerModel.imagePath)).image,
+                  ) ,
+                ),
+                Text(
+                  widget.winnerModel.imageInfo,
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 30)),
+                // 버튼 묶음
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // 다시하기 버튼
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => PlayWorldCupScreen(widget.worldCupModel, widget.round),
+                          ),
+                        );
+                      },
+                      style: const ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(Colors.red),
+                          shape: WidgetStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(5.0))
+                              )
+                          )
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.replay,
+                            color: Colors.white,
+                          ),
+                          Padding(padding: EdgeInsets.only(right: 10)),
+                          Text(
+                            '다시 하기',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 10)),
+                    // 공유 버튼
+                    IconButton(
+                        onPressed: () => shareGameWithKakao(),
+                        icon: Image.network(
+                          'https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png',
+                          width: 40,
+                        )
+                    ),
+                  ],
+                ),
+                // 팡파레 효과
+                Container(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ConfettiWidget(
+                        confettiController: _confettiController,
+                        blastDirection: -pi / 2 - 0.15,
+                        emissionFrequency: 0,
+                        numberOfParticles: 20,
+                        maxBlastForce: 120,
+                        minBlastForce: 60,
+                      ),
+                      ConfettiWidget(
+                        confettiController: _confettiController,
+                        blastDirection: -pi / 2 ,
+                        emissionFrequency: 0,
+                        numberOfParticles: 20,
+                        maxBlastForce: 120,
+                        minBlastForce: 60,
+                      ),
+                      ConfettiWidget(
+                        confettiController: _confettiController,
+                        blastDirection: -pi / 2 + 0.15,
+                        emissionFrequency: 0,
+                        numberOfParticles: 20,
+                        maxBlastForce: 120,
+                        minBlastForce: 60,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-            // 팡파레 효과
-            Container(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ConfettiWidget(
-                    confettiController: _confettiController,
-                    blastDirection: -pi / 2 - 0.15,
-                    emissionFrequency: 0,
-                    numberOfParticles: 20,
-                    maxBlastForce: 120,
-                    minBlastForce: 60,
-                  ),
-                  ConfettiWidget(
-                    confettiController: _confettiController,
-                    blastDirection: -pi / 2 ,
-                    emissionFrequency: 0,
-                    numberOfParticles: 20,
-                    maxBlastForce: 120,
-                    minBlastForce: 60,
-                  ),
-                  ConfettiWidget(
-                    confettiController: _confettiController,
-                    blastDirection: -pi / 2 + 0.15,
-                    emissionFrequency: 0,
-                    numberOfParticles: 20,
-                    maxBlastForce: 120,
-                    minBlastForce: 60,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+          if(isLoading) spinkit,
+        ],
+      ) ,
     );
   }
 
   // 카카오톡 공유하기 기능
   Future<void> shareGameWithKakao() async {
+    setState(() {
+      isLoading = true;
+    });
+
     var title = widget.worldCupModel.title;
     var image = File(widget.winnerModel.imagePath);
     // 바이너리 파일로 변환
@@ -180,5 +199,9 @@ class _ResultWorldCupScreen extends State<ResultWorldCupScreen> {
 
     var myTemplate = await makeFeedTemplate(title, description, imgUrl!, "");
     sendFeed(myTemplate);
+
+    setState(() {
+      isLoading = false;
+    });
   }
 }
