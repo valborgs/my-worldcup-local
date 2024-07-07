2ac690c46ac70ff27399cb6021f16ab3727b0a9f
valborgs <voll1212@naver.com>
2024-07-07 18:40:52 +0900

    [Fix] 라운드 만들어주는 함수 수정

--- lib/tools/make_round.dart
+++ lib/tools/make_round.dart
@@ -1,16 +1,17 @@
 // 최대 라운드 수에서 최소 4강까지 만들어주는 함수
 List<int> makeRoundList(int maxRound){
   List<int> tempList = [];
+  var round = 4;
   while(true){
-    if(maxRound ~/ 4 == 0) break; // 4강까지 만들어줌
-    if(maxRound ~/ 4 == 1) {
-      tempList.add(4);
-    }else{
-      tempList.add(maxRound ~/ 2);
+    if(maxRound ~/ 4 == 0) {
+      break; // 4강까지 만들어줌
+    }else {
+      tempList.add(round);
+      maxRound = maxRound~/2;
+      round*=2;
     }
-    maxRound = maxRound~/2;
   }
-  return tempList.reversed.toList();
+  return tempList;
 }
 
 // 전체 항목 개수에서 최대 라운드를 구해주는 함수
