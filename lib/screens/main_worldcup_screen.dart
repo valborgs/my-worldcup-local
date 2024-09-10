7b00060507d84b5191940046bdb8cf14cdd4208e
valborgs <voll1212@naver.com>
2024-09-10 21:57:32 +0900

    [Feat] 메인화면에서 뒤로가기 시 종료 다이얼로그 표시

--- lib/screens/main_worldcup_screen.dart
+++ lib/screens/main_worldcup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:my_worldcup_local/screens/help_screen.dart';
 
@@ -28,77 +29,117 @@ class _MainWorldCupScreenState extends State<MainWorldCupScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: Semantics(
-          label: "도움말 버튼",
-          button: true,
-          enabled: true,
-          child: IconButton(
-            tooltip: "도움말",
-            onPressed: (){
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => HelpScreen(false),
-                  fullscreenDialog: true,
-                ),
-              );
-            },
-            icon: const Icon(
-              Icons.help_outline,
-              semanticLabel: "도움말",
-              size: 24,
-            ),
-          ),
-        ),
-        title: const Text("내가 만든 월드컵", semanticsLabel: "내가 만든 월드컵 화면",),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: Semantics(
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        _showBackDialog();
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            leading: Semantics(
+              label: "도움말 버튼",
               button: true,
               enabled: true,
-              label: "Add WorldCup Button",
               child: IconButton(
-                onPressed: () {
+                tooltip: "도움말",
+                onPressed: (){
                   Navigator.of(context).push(
                     MaterialPageRoute(
-                      builder: (context) => const AddWorldCupScreen(),
+                      builder: (context) => HelpScreen(false),
                       fullscreenDialog: true,
                     ),
                   );
                 },
                 icon: const Icon(
-                  Icons.add,
-                  semanticLabel: "추가",
-                  size: 32,
+                  Icons.help_outline,
+                  semanticLabel: "도움말",
+                  size: 24,
                 ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: Column(
-          children: [
-            // 애드몹 배너
-            if(_bannerAd != null)
-              Semantics(
-                button: true,
-                enabled: true,
-                label: "Banner Ad",
-                child: SizedBox(
-                  width: _bannerAd?.size.width.toDouble(),
-                  height: _bannerAd?.size.height.toDouble(),
-                  child: AdWidget(ad: _bannerAd!),
+            title: const Text("내가 만든 월드컵", semanticsLabel: "내가 만든 월드컵 화면",),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Semantics(
+                  button: true,
+                  enabled: true,
+                  label: "Add WorldCup Button",
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const AddWorldCupScreen(),
+                          fullscreenDialog: true,
+                        ),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.add,
+                      semanticLabel: "추가",
+                      size: 32,
+                    ),
+                  ),
                 ),
               ),
-            // 월드컵 리스트
-            const WorldCupList(),
-          ],
+            ],
+          ),
+          body: Container(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Column(
+              children: [
+                // 애드몹 배너
+                if(_bannerAd != null)
+                  Semantics(
+                    button: true,
+                    enabled: true,
+                    label: "Banner Ad",
+                    child: SizedBox(
+                      width: _bannerAd?.size.width.toDouble(),
+                      height: _bannerAd?.size.height.toDouble(),
+                      child: AdWidget(ad: _bannerAd!),
+                    ),
+                  ),
+                // 월드컵 리스트
+                const WorldCupList(),
+              ],
+            ),
+          ),
         ),
-      ),
+    );
+  }
+
+  void _showBackDialog() {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('앱 종료'),
+          content: const Text(
+            '내가 만든 월드컵을 종료하시겠습니까?',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('아니오'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('네'),
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
